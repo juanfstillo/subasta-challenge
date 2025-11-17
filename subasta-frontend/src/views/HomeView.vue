8cfec20d78fad935088e9e92af6dc9978d074293
juanfstillo <juan.stillo1@gmail.com>
2025-11-17 00:00:37 -0300

    fix error en texto

--- subasta-frontend/src/views/HomeView.vue
+++ subasta-frontend/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="container text-center py-5">
     <img :src="logoSubasta" alt="Logo de Subasta" />
     <h1 class="mb-3 pt-1">Bienvenido a Subasta Online</h1>
-    <p class="mb-4">¡A la hora de realizar una subasta solo confíe sólo en el especialista!</p>
+    <p class="mb-4">¡A la hora de realizar una subasta confíe sólo en el especialista!</p>
     <router-link to="/subastas" class="btn btn-primary btn-lg">
       Ver Subastas
     </router-link>
